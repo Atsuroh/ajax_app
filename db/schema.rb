107816af6d281cc93aefd83f410d240c4cc6b7f4
Atsuroh <o89.yamanouchi.atsuro@gmail.com>
2020-12-06 21:05:00 +0900

    AjaxAppの準備

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_084748) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
+    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
